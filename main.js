eeaf0f4076eb4ab786a201f8b77bb20c61102c51
marwal1987 <144460382+marwal1987@users.noreply.github.com>
2024-08-23 07:46:24 +0200

    Refactored all

--- main.js
+++ main.js
@@ -1,40 +1,79 @@
-const submitBtn = document.getElementById('submit-btn');
+// Hämta det som behövs från DOM:en och spara i variabler
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const meddelandeInput = document.querySelector("textarea");
+const msgName = document.querySelector(".msgName");
+const msgEmail = document.querySelector(".msgEmail");
+const msgMeddelande = document.querySelector(".msgMeddelande");
+const userList = document.querySelector("#users");
 
-const validera = (e) => {
+const emailIsValid = (emailInput) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput);
+};
+// Lägg till ett submit event (eftersom det är ett formulär). onSubmit är en separat funktion som deklareras längre ner.
+
+myForm.addEventListener("submit", onSubmit);
+
+// Eftersom det är ett submit, vill vi använda preventDefault() för att förhindra att sidan laddas om innan resterande kod körs.
+function onSubmit(e) {
   e.preventDefault();
-  const namn = document.getElementById("namn");
-  const email = document.getElementById("email");
-  const meddelande = document.getElementById("meddelande")
 
-  if (namn.value === ""){
-    alert("Var god och fyll i ditt namn.");
-    username.focus();
-    return false;
-  }
-  if (email.value === "") {
-    alert("Var god fyll i din e-post.");
-    email.focus();
-    return false;
+  if (nameInput.value === "") {
+    msgName.classList.add("error");
+    msgName.innerHTML = "* Var god fyll i ditt namn";
+    setTimeout(() => msgName.remove(), 3000);
   }
 
-  if (!emailIsVaild(emailAddress.value)) {
-    alert("Var god och fyll i en giltig e-post adress.")
-    emailAddress.focus();
-    return false;
+  if (emailInput.value === "") {
+    msgEmail.classList.add("error");
+    msgEmail.innerHTML = "* Var god fyll i din Epost";
+    setTimeout(() => msgEmail.remove(), 3000);
+  } else if (!emailIsValid(emailInput.value)) {
+    msgEmail.classList.add("error");
+    msgEmail.innerHTML = "* Var god fyll i en giltig E-post adress";
+    setTimeout(() => msgEmail.remove(), 3000);
   }
 
-  if (meddelande.value === "") {
-    alert("Var god fyll i ett meddelande");
-    meddelande.focus();
-    return false;
+  if (meddelandeInput.value === "") {
+    msgMeddelande.classList.add("error");
+    msgMeddelande.innerHTML = "* Var god fyll ett meddelande";
+    setTimeout(() => msgMeddelande.remove(), 3000);
   }
 
-  return true // Formuläret är ifyllt och kan skickas till servern
+  // Om alla fält är ifyllda och e-posten är giltig, lägg till användaren i listan
+  if (
+    nameInput.value !== "" &&
+    emailIsValid(emailInput.value) &&
+    meddelandeInput.value !== ""
+  ) {
+    const li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(
+        `${nameInput.value} | ${emailInput.value} | ${meddelandeInput.value}`
+      )
+    );
 
+    userList.appendChild(li);
+
+    // Töm fälten
+    nameInput.value = "";
+    emailInput.value = "";
+    meddelandeInput.value = "";
+  }
 }
 
-const emailIsValid = email => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
+// Lägga till?
 
-submitBtn.addEventListener("click", validera);
+// {
+//   const li = document.createElement("li");
+//   li.appendChild(
+//     document.createTextNode(`${nameInput.value} | ${emailInput.value} | ${meddelandeInput.value}`)
+//   );
+
+//   userList.appendChild(li);
+
+//   // Clear fields
+//   nameInput.value = "";
+//   emailInput.value = "";
+//   meddelandeInput.value = "";
